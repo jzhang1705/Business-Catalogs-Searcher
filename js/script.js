39d961bc6363318a5f425875f0eb13e06cbe5ab3
edwarddu2001 <36455039+edwarddu2001@users.noreply.github.com>
2020-05-27 15:55:30 -0400

    Add css and js files

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+/*Initiate Animation on Hover*/
+function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+        function() {
+            element.addClass('animated ' + animation);
+        },
+        function(){
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass('animated ' + animation);
+            }, 2000);
+        });
+}
+/*Initiate Animation on Click*/
+function animationClick(element, animation){
+    element = $(element);
+    element.click(
+        function() {
+            element.addClass('animated ' + animation);
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass('animated ' + animation);
+            }, 2000);
+
+        });
+}
