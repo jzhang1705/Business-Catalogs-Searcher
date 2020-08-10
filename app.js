bce6710f62277b97e9f311fc634203896809d21a
ajayd3 <51806006+ajayd3@users.noreply.github.com>
2020-08-10 11:41:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -65,5 +65,55 @@ app.listen(port, () => console.log('listening at ' + port));
 app.use(express.static('public'));
 
 app.post('/api', (request, response) => {
-  console.log(request.body);
+  insertData(request.body);
 });
+
+
+function insertData(data){
+  'use strict'
+
+  var MongoClient = require('mongodb').MongoClient;
+  const uri = "mongodb+srv://VSC:528491@summer2020-qvltj.mongodb.net/BusinessCatalog?retryWrites=true&w=majority";
+  const bcrypt = require("bcrypt");
+  const saltRounds = 10;
+  const response = {statusCode: 200, body: JSON.stringify('Insert completed')};
+
+  let atlas_connection_uri;
+  let cachedDb = null;
+
+  exports.handler = (event, context, callback) => {
+          atlas_connection_uri = uri;
+          console.log('the Atlas connection string is ' + atlas_connection_uri);
+          processEvent(event, context, callback);
+  };
+
+  function processEvent(event, context, callback) {
+      console.log('Calling MongoDB Atlas from AWS Lambda with event: ' + JSON.stringify(event));
+      var jsonContents = JSON.parse(event.body);
+      //Used for inserting into database
+      MongoClient.connect(uri, function(err, db) {
+      if (err) throw err;
+          var dbo = db.db("BusinessCatalog");
+          console.log(event.body)
+          if(jsonContents.password !=null){
+          }
+          bcrypt
+          .genSalt(saltRounds)
+          .then(salt => {
+          console.log(`Salt: ${salt}`);
+          return bcrypt.hash(jsonContents.password, salt);
+          })
+          .then(hash => {
+          jsonContents.password = hash;
+          dbo.collection("logins").insertOne(jsonContents, function(err, res) {
+          if (err) throw err;
+          console.log("1 document inserted");
+          });
+          db.close();
+          })
+          });
+        
+          return response;
+
+  }
+}
